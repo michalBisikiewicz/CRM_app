f3d267b599cb86e9dce8521eb0d25cc2c4cdc339
Michał Bisikiewicz <bisikiewiczmichal@gmail.com>
2021-02-23 16:03:28 +0100

    added Password Reset Email functionality

--- CRM/settings.py
+++ CRM/settings.py
@@ -126,7 +126,19 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'accounts/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = '*****'
+EMAIL_HOST_PASSWORD = '*****'
+
+
--- accounts/urls.py
+++ accounts/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('account/', views.account_settings, name='account'),
-
 
     path('', views.home, name='home'),
     path('customer/<str:pk>/', views.customer, name='customer'),
@@ -19,6 +19,21 @@ urlpatterns = [
     path('update_order/<str:pk>/', views.updateOrder, name='update_order'),
     path('delete_order/<str:pk>/', views.deleteOrder, name='delete_order'),
 
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+         name="reset_password"),
+
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"),
+         name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"),
+         name="password_reset_confirm"),
+
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"),
+         name="password_reset_complete"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
